8bed69692e678a150872da3da720c16278fcacd8
Sneha_hsu <houshettisneha@gmail.com>
2026-04-05 19:38:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// Smooth scroll for navbar links
-document.querySelectorAll("a").forEach(anchor => {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href"))
-      .scrollIntoView({
-        behavior: "smooth"
-      });
-  });
-});
-
-// Button scroll
 function scrollToContact() {
   document.getElementById("contact").scrollIntoView({
     behavior: "smooth"
